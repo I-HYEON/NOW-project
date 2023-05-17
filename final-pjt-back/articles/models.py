c0d8f148058db689a6e19c5b3279fdcc5aa0a2fd
hw2ny1 <hw2ny1@gmail.com>
2023-05-17 23:07:16 +0900

    '_'

--- final-pjt-back/articles/models.py
+++ final-pjt-back/articles/models.py
@@ -6,7 +6,7 @@ class Article(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     title = models.CharField(max_length=50)
     content = models.TextField()
-    image = models.ImageField(blank=True, null=True)
+    # image = models.ImageField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- final-pjt-back/final_pjt_back/settings.py
+++ final-pjt-back/final_pjt_back/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'accounts',
     'articles',
     'deposits',
+    'corsheaders',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,3 +130,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+]
