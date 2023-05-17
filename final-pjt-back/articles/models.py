aa5677aa9624cb65cd2debc8ab2c9e4f94d8588f
hw2ny1 <hw2ny1@gmail.com>
2023-05-17 21:36:30 +0900

    '_'

--- final-pjt-back/articles/models.py
+++ final-pjt-back/articles/models.py
@@ -3,7 +3,7 @@ from accounts.models import User
 
 # Create your models here.
 class Article(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
     title = models.CharField(max_length=50)
     content = models.TextField()
     image = models.ImageField(blank=True, null=True)
@@ -11,8 +11,8 @@ class Article(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
-    user = models.ForeignKey(Article, on_delete=models.CASCADE)
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    article = models.ForeignKey(Article, on_delete = models.CASCADE)
     content = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
