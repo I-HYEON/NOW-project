70eafb1d47ba55376ead0097080ad0d6738b652c
I-HYEON <tjwn1408@naver.com>
2023-05-19 01:49:11 +0900

    ......

--- final-pjt-front/src/store/index.js
+++ final-pjt-front/src/store/index.js
@@ -7,6 +7,7 @@ const API_URL = 'http://127.0.0.1:8000'
 
 export default new Vuex.Store({
   state: {
+    token: null,
     articles: [
     ],
     comments: [
@@ -19,6 +20,10 @@ export default new Vuex.Store({
     // },
   },
   mutations: {
+    LOGIN(state, payload){
+      this.state.token = payload
+      console.log('잘들어갔나확인',this.state.token)
+    },
     GET_ARTICLES(state, articles){
       state.articles = articles
     },
@@ -56,20 +61,52 @@ export default new Vuex.Store({
       const username = payload.username
       const password1 = payload.password1
       const password2 = payload.password2
+      const age = payload.age
+      const gender = payload.gender
+      const salary = payload.salary
+      const wealth = payload.wealth
+      const tendency = payload.tendency
+      const email = payload.email
+      console.log(username, password1, password2, age, gender, salary, wealth, tendency, email)
 
       axios({
         method: 'post',
-        url: 'http://127.0.0.1:8000/accounts/signup/',
+        url: 'http://127.0.0.1:8000/accounts/registration/',
         data: {
-          username, password1, password2
+          username, password1, password2, age, gender, salary, wealth, tendency, email
         }
       })
       .then((res)=>{
-        console.log(res)
+        console.log('결과가',res)
+        console.log('토큰이!!',res.data.key)
       })
       .catch((err)=> {
         console.log(err)
       })
+    },
+    login(context, payload){
+      const username = payload.username
+      const password = payload.password
+      axios({
+        method: 'post',
+        url: 'http://127.0.0.1:8000/accounts/login/',
+        data: {
+          username, password
+        }
+      })
+      .then((res)=>{
+        // console.log(res.data.key)
+        const token = res.data.key
+        context.commit('LOGIN', token)
+        context.dispatch('saveTokenState')
+      })
+      .catch((err)=> {
+        console.log(err)
+      })
+    },
+    saveTokenState(context) {
+      const jsonToken = JSON.stringify(context.state.token)
+      localStorage.setItem('token', jsonToken)
     }
   },
   modules: {
