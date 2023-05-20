c1040872d94ef144d04e95735f2bafe9b5bff53c
hw2ny1 <hw2ny1@gmail.com>
2023-05-21 00:29:32 +0900

    끝!!

--- final-pjt-back/deposits/serializers.py
+++ final-pjt-back/deposits/serializers.py
@@ -13,3 +13,26 @@ class DepositProductsSerializer(serializers.ModelSerializer):
     class Meta:
         model = DepositProducts
         fields = '__all__'
+
+class DepositProductsSerializerC(serializers.ModelSerializer):
+    DepositOptions.fin_prdt_cd_set = DepositOptionsSerializer(many=True)
+    user_count = serializers.SerializerMethodField()
+
+    def get_user_count(self, obj):
+        temp = DepositProducts.objects.get(fin_prdt_cd=obj['fin_prdt_cd'])
+        return temp.joined.count()
+    
+    class Meta:
+        model = DepositProducts
+        fields = '__all__'
+
+class DepositProductsSerializerD(serializers.ModelSerializer):
+    DepositOptions.fin_prdt_cd_set = DepositOptionsSerializer(many=True)
+    user_count = serializers.SerializerMethodField()
+
+    def get_user_count(self, obj):
+        return obj.joined.count()
+    
+    class Meta:
+        model = DepositProducts
+        fields = '__all__'
